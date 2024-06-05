0c95370cc41ba4c494b4b21d433a3b12e944fa07
Tareq <razzbabut@gmail.com>
2024-06-05 17:54:59 +0600

    implement verifyAdmin for admin verification

--- index.js
+++ index.js
@@ -52,8 +52,20 @@ async function run() {
             })
         }
 
+        // use verify admin after verifyToken
+        const verifyAdmin = async (req, res, next) => {
+            const email = req.decoded.email;
+            const query = { email: email };
+            const user = await userCollection.findOne(query);
+            const isAdmin = user?.role === 'admin';
+            if (!isAdmin) {
+                return res.status(403).send({ message: 'forbidden access' })
+            }
+            next()
+        }
+
         // user related api
-        app.get('/users', async (req, res) => {
+        app.get('/users', verifyToken, verifyAdmin, async (req, res) => {
             const result = await userCollection.find().toArray();
             res.send(result);
         })
@@ -61,16 +73,16 @@ async function run() {
         app.get('/users/admin/:email', verifyToken, async (req, res) => {
             const email = req.params.email;
             if (email !== req.decoded.email) {
-              return res.status(403).send({ message: 'forbidden access' })
+                return res.status(403).send({ message: 'forbidden access' })
             }
             const query = { email: email };
             const user = await userCollection.findOne(query);
             let admin = false;
             if (user) {
-              admin = user?.role === 'admin'
+                admin = user?.role === 'admin'
             }
             res.send({ admin })
-          })
+        })
 
         app.post('/users', async (req, res) => {
             const user = req.body;
@@ -85,7 +97,7 @@ async function run() {
             res.send(result);
         })
 
-        app.patch('/users/admin/:id', async (req, res) => {
+        app.patch('/users/admin/:id', verifyToken, verifyAdmin, async (req, res) => {
             const id = req.params.id;
             const filter = { _id: new ObjectId(id) };
             const updatedDoc = {
